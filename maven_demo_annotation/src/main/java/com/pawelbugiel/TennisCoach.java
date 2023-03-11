2878bc6b8a0fc1887735f2115dc5659d0d57bc99
pawelbugiel <pawel.bugiel.7@gmail.com>
2023-03-11 10:14:08 +0100

    use SETTER DI with ANNOTATIONS.

--- maven_demo_annotation/src/main/java/com/pawelbugiel/TennisCoach.java
+++ maven_demo_annotation/src/main/java/com/pawelbugiel/TennisCoach.java
@@ -1,5 +1,7 @@
 package com.pawelbugiel;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -9,12 +11,23 @@ public class TennisCoach implements Coach {
 
 	// == fields ==
 	private FortuneService fortuneService;
+	private static final Logger log = LoggerFactory.getLogger(TennisCoach.class);
 
 	// == constructors ==
+	
+	public TennisCoach() {
+		System.out.println("Tennis coach default constructor called..");
+	};
 	// if a class have only one constructor 
 	//@Autowired annotation is redundant till Spring 4.3
-	@Autowired				 
-	public TennisCoach(FortuneService fortuneService) {
+//	@Autowired				 
+//	public TennisCoach(FortuneService fortuneService) {
+//		this.fortuneService = fortuneService;
+//	}
+	
+	@Autowired
+	public void setFortuneService(FortuneService fortuneService) {
+		log.info(" Log from Tennis Coach, setter method ");
 		this.fortuneService = fortuneService;
 	}
 	
